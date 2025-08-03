e8bf30b875ea9cc476fd17b2d6c3468950617a20
himel2535 <monwarhossanhimel@gmail.com>
2025-08-04 00:33:15 +0600

    problem4 solved about split

--- problem4.js
+++ problem4.js
@@ -1,4 +1,14 @@
 /** Problem 04 - (Delete / Store) */
-var fileName= "pdfData.jpg";
+var fileName = "Data.jpg";
 //write your code here
 
+var splitted = fileName.split(".");
+if (
+  splitted[splitted.length - 1] === "pdf" ||
+  splitted[splitted.length - 1] === "docs" ||
+  fileName[0] === "#"
+) {
+  console.log("Store");
+} else {
+  console.log("Delete");
+}
