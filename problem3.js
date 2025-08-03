d66e4353b9a1840e0d07fa16b374b55e9802be99
himel2535 <monwarhossanhimel@gmail.com>
2025-08-04 00:03:14 +0600

    problem 3 task using for loop

---
+++ problem3.js
@@ -0,0 +1,11 @@
+/** Problem -03 ( Medicine Planner ) */
+var lastDay = 6 ;
+//write your code here
+for(i=1;i<=lastDay;i++){
+    if(i%3===0){
+        console.log('medicine')
+    }
+    else{
+        console.log('rest')
+    }
+}
